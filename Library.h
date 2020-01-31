772c09b2671ab949eb392bf45bbae0b648d4ebe1
lcayia <lcayia@uvm.edu>
2020-01-31 17:40:48 -0500

    Added RME comments, updated README.md

--- Library.h
+++ Library.h
@@ -1,5 +1,6 @@
 #ifndef LC_PROJECT2_LIBRARY_H
 #define LC_PROJECT2_LIBRARY_H
+#include <algorithm>
 #include <memory>
 #include <iostream>
 #include <optional>
@@ -11,7 +12,7 @@ using namespace std;
 
 // TODO: Implement optional stuff
 // TODO: Implement smart pointers
-// TODO: Implement functions
+// TODO: Implement addBook and buyBook
 
 class Book {
 private:
@@ -25,10 +26,16 @@ private:
 
 public:
     // Constructor
+    // Requires: Book no., title, author, page count, and price (if applicable)
+    // Modifies: Nothing
+    // Effects: Creates a Book object
     Book(int num, string title, string author, int pages);
     Book(int num, string title, string author, int pages, double price);
 
     // Getters
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Effects: Returns the value specified by the function
     int getNum() const;
     string getTitle() const;
     string getAuthor() const;
@@ -38,6 +45,9 @@ public:
     optional<string> getPurchasePrice() const;
 
     // Setters
+    // Requires: The data that the field specified should be changed to
+    // Modifies: The field(s) specified by the function
+    // Effects: Modifies certain fields based on the function
     void setBookInfo(string title, string author, int pages);
     void setISBN(string isbn);
     void setAvailability(bool status);
@@ -50,15 +60,39 @@ private:
 
 public:
     // Constructor
+    // Requires: Nothing or a vector of books to initialize the library with
     Library();
     Library(vector<Book> inv);
 
-    bool searchForBook(); // By Name, Author, ISBN, or Page Count Range
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Effects: Allows the user to search for a book by title or by author
+    bool searchForBook();
+
+    // Requires: A Book object
+    // Modifies: Nothing
+    // Effects: Displays a book's number, name, author, page count, and availability to the user
     void getBookInfo(Book book) const;
+
+    // Requires: Nothing
+    // Modifies: isAvailable field
+    // Effects: Allows the user to check a book out, making it not available
     void checkOut();
+
+    // Requires: Nothing
+    // Modifies: isAvailable field
+    // Effects: Allows the user to check a book in, making it available
     void checkIn();
+
+    // Requires: TBD
+    // Modifies: TBD
+    // Effects: TBD
     void addBook();
-    void buyBook(Book book);
+
+    // Requires: TBD
+    // Modifies: TBD
+    // Effects: TBD
+    void buyBook();
 };
 
 #endif //LC_PROJECT2_LIBRARY_H
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# LC-Project2
+# Landon Cayia - Project 2
+My second CS120 Project
+
+## First Part
+
+#### Why did you choose this class?
+I chose this class because I felt that a system for managing library books would
+be more applicable for the purposes of this assignment. If code were reused from
+Project 1, it would be necessary to write another global function that used my
+`HangmanGame` class. I could not think of a way to use the class for anything
+other than its basic functionality, so I wrote a new class instead. I also felt
+inspired to do something different that may teach me C++ techniques that I did
+not learn in lecture.
+
+#### Why does it make sense for the purchasePrice field to be an optional?
+It makes sense for the `purchasePrice` field to be an optional because not every
+book in the library is available for purchase. It it useful to be able to know
+1) Whether the field was initialized or not, to know whether the book is for sale.
+2) The value of the field if it was initialized, so that the user can know the
+purchase price of the book.
+
+If an optional were not used here, this would require two separate variables: a
+`bool` for whether the book is for sale or not, and a `double` for its price.
+A field of type `optional<double>` covers both of these bases.
+
+#### Why did I use a smart pointer for _____?
+
+
+#### How did I use the class with a function?
+I wrote a global function called `initializeLibrary` that reads a csv file
+containing information about numerous library books and adds them to a vector
+that stores the books in an instance of the library class, called bookInventory.
+
+## Second Part
+This part just involves writing test cases. Not sure whether I need to add
+anything to README.md or not.
+
+## Third Part
+
+#### Did all test cases pass on the first try?
+Write stuff here
+
+## Fourth Part
+
+#### 
+
+## Fifth Part
+
+
